5dd935b14f625fc5f94035f627d3e111a5061374
Jonathan Galdino <johnzballad@gmail.com>
2023-05-25 06:27:52 -0300

    feat: basic resolver for fetching todos

--- packages/server/src/resolvers/todo/todo.resolvers.ts
+++ packages/server/src/resolvers/todo/todo.resolvers.ts
@@ -1,10 +1,30 @@
 import type { QueryResolvers } from '@core/schemas/__generated__/graphql';
 
 export const Query: QueryResolvers = {
-  todo: async (_, args, _context) => ({
-      id: args.id.toString(),
-      name: 'Random XD',
-      description: 'random todo',
-      completed: false,
-    })
+  todo: (_, args, _context) => ({
+    id: args.id.toString(),
+    name: 'Random XD',
+    description: 'random todo',
+    completed: false,
+  }),
+  todos: (_, _args, _context) => ({
+    edges: [
+      {
+        node: {
+          id: '1',
+          name: 'Random XD',
+          description: 'random todo',
+          completed: false,
+        }
+      },
+      {
+        node: {
+          id: '2',
+          name: 'Random XD',
+          description: 'random todo',
+          completed: false,
+        }
+      }
+    ]
+  })
 };
