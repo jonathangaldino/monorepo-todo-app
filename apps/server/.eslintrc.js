687f9f148f02dda94a2f34ac5e5509d16513078b
Jonathan Galdino <johnzballad@gmail.com>
2023-05-30 23:57:04 -0300

    lint(eslint); ignoring /build folders

--- apps/server/.eslintrc.js
+++ apps/server/.eslintrc.js
@@ -2,10 +2,11 @@ module.exports = {
   root: true,
   env: { node: true },
   parser: '@typescript-eslint/parser',
-  extends: ["custom"],
+  extends: ['custom'],
+  ignorePatterns: ['**/build/*'],
   rules: {
-    "@typescript-eslint/no-namespace": "off",
-    "@typescript-eslint/no-explicit-any": "off",
-    "@typescript-eslint/no-unused-vars": "off",
+    '@typescript-eslint/no-namespace': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
   },
 }
